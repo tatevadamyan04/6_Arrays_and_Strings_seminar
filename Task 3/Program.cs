019ee57fd4975a9b3462cf5a9bfb6e1c541ddbd6
Tatevik Adamyan <tatevadamyan2004@gmail.com>
2023-12-25 01:08:10 +0400

    Task  is done

---
+++ Task 3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 3: Проверка строки на палиндром
+public class Task3
+{
+    public static void Main(string[] args)
+    {
+        string input = "level";
+        bool isPalindrome = IsPalindrome(input);
+        Console.WriteLine(isPalindrome ? "yes" : "no");
+    }
+    public static bool IsPalindrome(string str)
+    {
+        string normalized = new
+        string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+        return normalized.SequenceEqual(normalized.Reverse());
+    }
+}
