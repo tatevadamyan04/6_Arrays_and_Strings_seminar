0682c398d4cc8d72739b50fbf217a19bb9a2e09c
Tatevik Adamyan <tatevadamyan2004@gmail.com>
2023-12-25 01:14:20 +0400

    Task 4 is done

---
+++ Task 4/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 4 (необязательная): Обратный порядок слов в строке
+public class Task4
+{
+    public static void Main(string[] args)
+    {
+
+        string input = "Hello world";
+
+        string result = ReverseWords(input);
+
+        Console.WriteLine(result);
+    }
+
+    public static string ReverseWords(string str)
+    {
+
+        string[] words = str.Split(' ');
+
+        Array.Reverse(words);
+
+        return string.Join(" ", words);
+    }
+}
