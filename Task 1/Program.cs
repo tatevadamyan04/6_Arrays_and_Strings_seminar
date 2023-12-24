1642aa80673fb0dfca6ac8564e6ed5d31f1acf99
Tatevik Adamyan <tatevadamyan2004@gmail.com>
2023-12-25 00:52:14 +0400

    initial commit

---
+++ Task 1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+public class Task1
+{
+    public static void Main(string[] args)
+    {
+        char[,] charArray = new char[,] { { 'a', 'b' }, { 'c', 'd' } };
+        string result = CreateStringFrom2DArray(charArray);
+        Console.WriteLine(result);
+    }
+    public static string CreateStringFrom2DArray(char[,] array)
+    {
+        string result = "";
+        for (int i = 0; i < array.GetLength(0); i++)
+            for (int j = 0; j < array.GetLength(1); j++)
+                result += array[i, j];
+        return result;
+    }
+}
