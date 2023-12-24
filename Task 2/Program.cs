7843ed58f43fa7b635970abc529598f40ae68544
Tatevik Adamyan <tatevadamyan2004@gmail.com>
2023-12-25 00:58:40 +0400

    Task 2 is done

---
+++ Task 2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 2: Замена заглавных букв на строчные в строке
+public class Task2
+{
+    public static void Main(string[] args)
+    {
+        string input = "aBcD1ef!-";
+        string result = input.ToLower();
+        Console.WriteLine(result);
+    }
+}
